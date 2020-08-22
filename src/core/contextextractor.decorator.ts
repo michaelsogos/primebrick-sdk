1c5e3ec77547b596839b07c532e0fd28fda735a8
Michael Sogos <michael.sogos@gurustudioweb.it>
2020-08-22 09:48:33 +0200

    * Fixed bug "undefined object" on @Context decorator params

--- src/core/contextextractor.decorator.ts
+++ src/core/contextextractor.decorator.ts
@@ -7,7 +7,7 @@ import { CommonHelper } from "./utils/CommonHelper";
 export const Context = createParamDecorator((data: { excludeUserProfile?: boolean }, ctx: ExecutionContext) => {
 	const result = new ContextPayload();
 	result.tenantAlias = TenantManagerHelper.getTenantAliasFromContext(ctx);
-	result.userProfile = data.excludeUserProfile ? null : AuthManagerHelper.getUserProfile(ctx);
+	result.userProfile = data && data.excludeUserProfile ? null : AuthManagerHelper.getUserProfile(ctx);
 	result.languageCode = CommonHelper.getLanguageCode(ctx, result.userProfile);
 	return result;
 });
