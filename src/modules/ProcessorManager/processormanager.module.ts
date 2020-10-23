9e7df562496fffcc157d1256ae63aab19fa11482
Michael Sogos <michael.sogos@gurustudioweb.it>
2020-10-23 20:54:00 +0200

    * Fixed import of SessionManagerModule

--- src/modules/ProcessorManager/processormanager.module.ts
+++ src/modules/ProcessorManager/processormanager.module.ts
@@ -1,15 +1,12 @@
 import { Module } from '@nestjs/common';
 import { ClientsModule, Transport } from '@nestjs/microservices';
+import { SessionManagerModule } from '../SessionManager/sessionmanager.module';
 import { ProcessorManagerService } from './processormanager.service';
 
 @Module({
-  imports: [
-    ClientsModule.register([
-      { name: 'PRIMEBRICK_SERVICE', transport: Transport.TCP },
-    ]),
-  ],
-  controllers: [],
-  providers: [ProcessorManagerService],
-  exports: [ProcessorManagerService],
+    imports: [ClientsModule.register([{ name: 'PRIMEBRICK_SERVICE', transport: Transport.TCP }]), SessionManagerModule],
+    controllers: [],
+    providers: [ProcessorManagerService],
+    exports: [ProcessorManagerService],
 })
 export class ProcessorManagerModule {}
