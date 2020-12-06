513177c859648d570a130717e79320bcb992cf01
Michael Sogos <michael.sogos@gurustudioweb.it>
2020-12-06 18:11:15 +0100

    * Fixed issue with AudibleEntitySubscriber in order to handle auditing fields properly with typeorm save() and softRemove() methods * Improved TenantRepositoryService overriding softDelete(), insert(), update() methods in order to handle auditing fields for direct-to-db changes

--- src/modules/TenantManager/tenantrepository.service.ts
+++ src/modules/TenantManager/tenantrepository.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@nestjs/common';
-import { Repository, getConnectionManager, createConnection, getRepository, Connection } from 'typeorm';
+import { Repository, getConnectionManager, createConnection, getRepository, Connection, ObjectID, FindConditions, EntityTarget } from 'typeorm';
 import { Tenant } from '../TenantManager/entities/Tenant.entity';
 import { OptimisticLockingSubscriber } from '../../db/events/optimisticLocking.subscriber';
 import { TenantManagerHelper } from './utils/TenantManagerHelper';
@@ -7,6 +7,8 @@ import { SnakeNamingStrategy } from '../../db/namingStrategies/SnakeNamingStrate
 import { AudibleEntitySubscriber } from '../../db/events/audibleentity.subscriber';
 import { SessionManagerContext } from '../SessionManager/sessionmanager.context';
 import { SessionContext } from '../../core';
+import { QueryDeepPartialEntity } from 'typeorm/query-builder/QueryPartialEntity';
+import { AudibleEntity } from '../../db';
 
 @Injectable()
 export class TenantRepositoryService {
@@ -20,7 +22,9 @@ export class TenantRepositoryService {
 
     async getTenantRepository<TEntity>(entity: new () => TEntity): Promise<Repository<TEntity>> {
         const connection = await this.getTenantConnection();
-        return getRepository<TEntity>(entity, connection.name);
+        // const repository = getRepository<TEntity>(entity, connection.name);
+
+        return connection.getRepository<TEntity>(entity);
     }
 
     async getTenantConnection(): Promise<Connection> {
@@ -47,9 +51,10 @@ export class TenantRepositoryService {
         if (!tenant) throw new Error('Cannot create connecton to database because [Tenant] is null!');
 
         const connectionManager = getConnectionManager();
+        let conn: Connection = null;
 
         if (!connectionManager.has(tenant.code)) {
-            const conn = await createConnection({
+            conn = await createConnection({
                 name: tenant.code,
                 type: 'postgres',
                 host: tenant.tenant_db_config.db_host,
@@ -67,9 +72,74 @@ export class TenantRepositoryService {
                 entityPrefix: `${global['appModuleName']}_`,
             });
             conn.subscribers.push(this.audibleEntitySubscriber);
-            return conn;
         } else {
-            return connectionManager.get(tenant.code);
+            conn = connectionManager.get(tenant.code);
         }
+
+        const self = this;
+        conn.getRepository = function <TEntity>(entity: EntityTarget<TEntity>) {
+            return self.getSuperRepository<TEntity>(entity, conn.name);
+        };
+        return conn;
+    }
+
+    getSuperRepository<TEntity>(entity, connectionName) {
+        const repository = getRepository<TEntity>(entity, connectionName);
+        const sessionContext = this.sessionManagerContext;
+
+        const __softDelete = repository.softDelete;
+        repository.softDelete = async function (
+            criteria: string | number | string[] | number[] | Date | Date[] | ObjectID | ObjectID[] | FindConditions<TEntity>,
+        ) {
+            let deletedBy = -1;
+            try {
+                const context = sessionContext.get('context');
+                deletedBy = context.userProfile ? context.userProfile.id : -1;
+            } catch (ex) {
+                deletedBy - 1;
+            }
+            const entity: QueryDeepPartialEntity<unknown> = { deletedBy: deletedBy };
+            await repository.update(criteria, entity);
+            return __softDelete(criteria);
+        };
+
+        const __update = repository.update;
+        repository.update = async function (
+            criteria: string | number | string[] | number[] | Date | Date[] | ObjectID | ObjectID[] | FindConditions<TEntity>,
+            partialEntity: QueryDeepPartialEntity<TEntity>,
+        ) {
+            let updatedBy = -1;
+            try {
+                const context = sessionContext.get('context');
+                updatedBy = context.userProfile ? context.userProfile.id : -1;
+            } catch (ex) {
+                updatedBy - 1;
+            }
+
+            partialEntity['updatedBy'] = updatedBy;
+
+            return __update(criteria, partialEntity);
+        };
+
+        const __insert = repository.insert;
+        repository.insert = async function (entity: QueryDeepPartialEntity<TEntity> | QueryDeepPartialEntity<TEntity>[]) {
+            let createdBy = -1;
+            let updatedBy = -1;
+            try {
+                const context = sessionContext.get('context');
+                createdBy = context.userProfile ? context.userProfile.id : -1;
+                updatedBy = context.userProfile ? context.userProfile.id : -1;
+            } catch (ex) {
+                createdBy - 1;
+                updatedBy - 1;
+            }
+
+            entity['createdBy'] = createdBy;
+            entity['updatedBy'] = updatedBy;
+
+            return __insert(entity);
+        };
+
+        return repository;
     }
 }
